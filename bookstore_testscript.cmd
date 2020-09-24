759a5abe4835d3d6e1b13d8332d0e172070994c5
gtrofimov13 <36088333+gtrofimov13@users.noreply.github.com>
2020-09-23 23:39:30 -0700

    Add files via upload

---
+++ bookstore_testscript.cmd
@@ -0,0 +1,19 @@
+# Set LTC Report Home
+#var report-root = C:\CONTINUOUS_LOAD_TEST
+
+
+# Set Project Info
+var project = Bookstore-Consistency
+var test-name = bookstore
+
+
+# Set the variable values according to your needs
+var scenario = "Steady Load"
+#var base = C:/Parasoft/loadtest/git/
+var category = BackComp.SOAtest.Consistency
+var minutes = 5
+#var localsetting = C:/Parasoft/loadtest/lt.properties
+
+# Run Test
+open ${test-name}.lt
+loadtest -minutes ${minutes} -allReports ${report-root}/${project}/%d/${category}/${test-name} ${scenario}
